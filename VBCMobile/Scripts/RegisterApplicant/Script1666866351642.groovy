adbc8c058ea2488616059809fcd929da11ba2646
abhinkrishna-ms <abhin.krishna@mangosciences.com>
2022-10-28 12:14:28 +0530

    Completed Applicant-Registration

--- VBCMobile/Scripts/RegisterApplicant/Script1666866351642.groovy
+++ VBCMobile/Scripts/RegisterApplicant/Script1666866351642.groovy
@@ -17,3 +17,52 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
+Mobile.startExistingApplication('com.mangosciences.vbc')
+
+//Mobile.setText(findTestObject('Object Repository/Register-Applicant/android.widget.EditText'), '2431341212', 0)
+//
+//Mobile.setText(findTestObject('Object Repository/Register-Applicant/android.widget.EditText (1)'), '422781', 0)
+//
+//Mobile.tap(findTestObject('Object Repository/Register-Applicant/android.widget.TextView - Continue'), 0)
+//
+//Mobile.setText(findTestObject('Object Repository/Register-Applicant/android.widget.EditText (2)'), 'Pass@123', 0)
+//
+//Mobile.setText(findTestObject('Object Repository/Register-Applicant/android.widget.EditText (3)'), 'Pass@123', 0)
+//
+//Mobile.tap(findTestObject('Object Repository/Register-CompleteProfile/android.widget.TextView - Continue (1)'), 0)
+//
+//Mobile.comment('Add DOB')
+//
+//WebUI.delay(30)
+Mobile.scrollToText('address')
+
+Mobile.setText(findTestObject('Object Repository/Register-Applicant/android.widget.EditText (4)'), '#30,ojhc8whve,ewfvoefh', 
+    0)
+
+Mobile.tap(findTestObject('Object Repository/Register-Applicant/android.widget.TextView-Country'), 0)
+
+Mobile.tap(findTestObject('Object Repository/Register-Applicant/android.widget.TextView - India'), 0)
+
+Mobile.tap(findTestObject('Object Repository/Register-Applicant/android.widget.TextView-State'), 0)
+
+Mobile.selectListItemByLabel(findTestObject('Object Repository/Register-Applicant/android.widget.TextView - Chandigarh'), 
+    'Assam', 0)
+
+Mobile.tap(findTestObject('Object Repository/Register-Applicant/android.widget.TextView-City'), 0)
+
+Mobile.tap(findTestObject('Object Repository/Register-Applicant/android.widget.TextView - Chandigarh (1)'), 0)
+
+Mobile.setText(findTestObject('Object Repository/Register-Applicant/android.widget.EditText-pinCode'), '654322', 0)
+
+Mobile.tap(findTestObject('Object Repository/Register-Applicant/android.widget.TextView - Checkbox Address'), 0)
+
+Mobile.scrollToText('Continue')
+
+Mobile.setText(findTestObject('Object Repository/Register-Applicant/android.widget.EditText-PAN'), 'NATYI8919K', 0)
+
+Mobile.tap(findTestObject('Object Repository/Register-Applicant/android.widget.TextView - Checkbox Terms of Use'), 0)
+
+Mobile.tap(findTestObject('Register-Applicant/android.widget.TextView - Continue (2)'), 0)
+
+Mobile.closeApplication()
+
